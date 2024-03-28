748f42fc0ba0628812042c5efe5f0870b3aeffb2
freddieb77 <fred.barrett+github@gmail.com>
2024-03-28 16:51:05 -0500

    combined reducer for search into store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
+import searchReducer from './searchReducer';
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ 
+    reducer: rootReducer,
+    search: searchReducer,
+});
 
 export default store;
 
