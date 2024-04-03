31a924f23ff52149e642812e98d9fa6f40bc41be
freddieb77 <fred.barrett+github@gmail.com>
2024-04-03 10:58:44 -0500

    Restarting Project

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import RedditFeed from './components/RedditFeed/redditFeed';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Provider store={store}>
-      <RedditFeed />
-    </Provider>
   </React.StrictMode>
 );
 
--- src/redux/redditFeedActions.js
+++
@@ -1,6 +0,0 @@
-export const SET_POSTS = 'SET_POSTS';
-
-export const setPosts = (posts) => ({
-    type: SET_POSTS,
-    payload: posts,
-});
